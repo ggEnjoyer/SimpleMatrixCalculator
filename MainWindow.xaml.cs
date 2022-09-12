16d76b3db2a6caa624a99a45ec6e80d4fb57d4c9
Илья К <watttt311@gmail.com>
2022-09-13 01:20:35 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatrixCalculator1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Matrix1.ColumnDefinitions.Add(new ColumnDefinition());
+            Matrix1.ColumnDefinitions.Add(new ColumnDefinition());
+            Matrix1.ColumnDefinitions.Add(new ColumnDefinition());
+            Matrix1.RowDefinitions.Add(new RowDefinition());
+            Matrix1.RowDefinitions.Add(new RowDefinition());
+
+            var myUserControl = new TextBox();
+            Matrix1.Children.Add(myUserControl);
+            Grid.SetRow(myUserControl, 1);
+        }
+    }
+}
