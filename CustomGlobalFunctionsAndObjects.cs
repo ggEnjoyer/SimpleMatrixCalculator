71a537274d9fbc0a643f78241fd2c76018b73a45
Илья К <watttt311@gmail.com>
2022-09-13 12:54:41 +0300

    -Basic structure -Sum functionality

---
+++ CustomGlobalFunctionsAndObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalculator1
+{
+    internal static class CustomGlobalFunctionsAndObjects
+    {
+        [Conditional("DEBUG")]
+        public static void DebugMessage(string message)
+        {
+            System.Windows.MessageBox.Show(message);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+using static MatrixCalculator1.CustomGlobalFunctionsAndObjects;
 
 namespace MatrixCalculator1
 {
@@ -20,18 +24,208 @@ namespace MatrixCalculator1
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isFirstStart = true; 
         public MainWindow()
         {
             InitializeComponent();
-            Matrix1.ColumnDefinitions.Add(new ColumnDefinition());
-            Matrix1.ColumnDefinitions.Add(new ColumnDefinition());
-            Matrix1.ColumnDefinitions.Add(new ColumnDefinition());
-            Matrix1.RowDefinitions.Add(new RowDefinition());
-            Matrix1.RowDefinitions.Add(new RowDefinition());
+            Thread.Sleep(5000);
+            ChangeSizeOfMatrix1();
+            ChangeSizeOfMatrix2();
+            ChangeSizeOfMatrixResult();
+        }
+        void ChangeSizeOfMatrix1()
+        {
 
-            var myUserControl = new TextBox();
-            Matrix1.Children.Add(myUserControl);
-            Grid.SetRow(myUserControl, 1);
+            try
+            {
+                Matrix1.ColumnDefinitions.Clear();
+                Matrix1.RowDefinitions.Clear();
+                Matrix1.Children.Clear();
+
+                int rows = Convert.ToInt32(Matrix1Rows.Value);
+                int columns = Convert.ToInt32(Matrix1Columns.Value);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    Matrix1.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                }
+                for (int j = 0; j < columns; j++)
+                {
+                    Matrix1.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                }
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        //var matrixElement = new IntegerUpDown { AllowTextInput = true, Value = 0 };
+                        var matrixElement = new MatrixElement();
+                        
+                        //matrixElement.Style.Setters.Add(new Setter(IntegerUpDown.ShowButtonSpinnerProperty, false));
+                        //matrixElement.Style.Setters.Add(new Setter(PaddingProperty, new Thickness(0, 0, 16, 0)));
+                        //matrixElement.Style.Setters.Add(new Setter(WidthProperty, 40));
+                        /*
+                        var asd = new Trigger()
+                        {
+                            Property = IntegerUpDown.ShowButtonSpinnerProperty,
+                            Value = true
+                        };
+                        asd.Setters.Add(new Setter(PaddingProperty, new Thickness(0)));
+                        asd.Setters.Add(new Setter(IntegerUpDown.ShowButtonSpinnerProperty, true));
+                        
+                        matrixElement.Style.Triggers.Add(asd);
+                        */
+                        Matrix1.Children.Add(matrixElement);
+                        Grid.SetRow(matrixElement, i);
+                        Grid.SetColumn(matrixElement, j);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+        void ChangeSizeOfMatrix2()
+        {
+
+            try
+            {
+                Matrix2.ColumnDefinitions.Clear();
+                Matrix2.RowDefinitions.Clear();
+                Matrix2.Children.Clear();
+
+                int rows = Convert.ToInt32(Matrix1Rows.Value);
+                int columns = Convert.ToInt32(Matrix1Columns.Value);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    Matrix2.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                }
+                for (int j = 0; j < columns; j++)
+                {
+                    Matrix2.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                }
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        //var matrixElement = new IntegerUpDown { AllowTextInput = true, Value = 0 };
+                        var matrixElement = new MatrixElement();
+
+                        //matrixElement.Style.Setters.Add(new Setter(IntegerUpDown.ShowButtonSpinnerProperty, false));
+                        //matrixElement.Style.Setters.Add(new Setter(PaddingProperty, new Thickness(0, 0, 16, 0)));
+                        //matrixElement.Style.Setters.Add(new Setter(WidthProperty, 40));
+                        /*
+                        var asd = new Trigger()
+                        {
+                            Property = IntegerUpDown.ShowButtonSpinnerProperty,
+                            Value = true
+                        };
+                        asd.Setters.Add(new Setter(PaddingProperty, new Thickness(0)));
+                        asd.Setters.Add(new Setter(IntegerUpDown.ShowButtonSpinnerProperty, true));
+                        
+                        matrixElement.Style.Triggers.Add(asd);
+                        */
+                        Matrix2.Children.Add(matrixElement);
+                        Grid.SetRow(matrixElement, i);
+                        Grid.SetColumn(matrixElement, j);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+        private void ChangeSizeOfMatrixResult()
+        {
+
+            try
+            {
+                MatrixResult.ColumnDefinitions.Clear();
+                MatrixResult.RowDefinitions.Clear();
+                MatrixResult.Children.Clear();
+
+                int rows = Convert.ToInt32(Matrix1Rows.Value);
+                int columns = Convert.ToInt32(Matrix1Columns.Value);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    MatrixResult.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                }
+                for (int j = 0; j < columns; j++)
+                {
+                    MatrixResult.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                }
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        //var matrixElement = new IntegerUpDown { AllowTextInput = true, Value = 0 };
+                        var matrixElement = new MatrixElement();
+
+                        //matrixElement.Style.Setters.Add(new Setter(IntegerUpDown.ShowButtonSpinnerProperty, false));
+                        //matrixElement.Style.Setters.Add(new Setter(PaddingProperty, new Thickness(0, 0, 16, 0)));
+                        //matrixElement.Style.Setters.Add(new Setter(WidthProperty, 40));
+                        /*
+                        var asd = new Trigger()
+                        {
+                            Property = IntegerUpDown.ShowButtonSpinnerProperty,
+                            Value = true
+                        };
+                        asd.Setters.Add(new Setter(PaddingProperty, new Thickness(0)));
+                        asd.Setters.Add(new Setter(IntegerUpDown.ShowButtonSpinnerProperty, true));
+                        
+                        matrixElement.Style.Triggers.Add(asd);
+                        */
+                        MatrixResult.Children.Add(matrixElement);
+                        Grid.SetRow(matrixElement, i);
+                        Grid.SetColumn(matrixElement, j);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+        private void Matrix1Rows_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            ChangeSizeOfMatrix1();
+            ChangeSizeOfMatrix2();
+            ChangeSizeOfMatrixResult();
+        }
+
+        private void Matrix1Columns_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            ChangeSizeOfMatrix1();
+            ChangeSizeOfMatrix2();
+            ChangeSizeOfMatrixResult();
+        }
+
+
+        private void SumOfMatricesButton_Click(object sender, RoutedEventArgs e)
+        {
+            int rows = Convert.ToInt32(Matrix1Rows.Value);
+            int columns = Convert.ToInt32(Matrix1Columns.Value);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    MatrixElement ME = (MatrixElement)MatrixResult.Children[j + i * columns];
+                    MatrixElement M1 = (MatrixElement)Matrix1.Children[j + i * columns];
+                    MatrixElement M2 = (MatrixElement)Matrix2.Children[j + i * columns];
+                    //ME.ValueHolder.Value;
+                    //MatrixResult.Children[j + i * columns] = Matrix1.Children[j + i * columns] + Matrix2.Children[j + i * columns];
+                    ((MatrixElement)MatrixResult.Children[j + i * columns]).ValueHolder.Value = M1.ValueHolder.Value + M2.ValueHolder.Value;
+                    MatrixResult.Children[j + i * columns].SetValue(IntegerUpDown.ValueProperty, M1.ValueHolder.Value + M2.ValueHolder.Value);
+                    DebugMessage(M1.ValueHolder.Value.ToString() + " + " + M2.ValueHolder.Value.ToString() + " = " + (M1.ValueHolder.Value + M2.ValueHolder.Value).ToString());
+                }
+            }
         }
     }
 }
